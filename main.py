53c0fa683eb6781481dd707a1fa82914d1fd89cd
Liam <liam3323@gmail.com>
2024-10-13 18:51:09 +0100

    added main

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client()
+# Create an Intents object with all intents enabled
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():

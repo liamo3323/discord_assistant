71210d8dc30264c5f450a1a81bd4fafd49571e17
Liam <liam3323@gmail.com>
2024-10-22 23:56:51 +0100

    tried adding asyns

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # bot.py
-import time
+
 import os
 import discord
-from discord.ext import commands
-from dotenv import load_dotenv
 import logging
 import yaml
+import asyncio
+
+from web_yoinking import get_games_info
+from discord.ext import commands
+from dotenv import load_dotenv
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -15,12 +19,18 @@ intents = discord.Intents.default()
 intents.message_content = True 
 client = discord.Client(intents=intents)
 
+async def web_yoink():
+    print("Yoinking...")
+    get_games_info()
+    await asyncio.sleep(30)
+    print("30 seconds later")
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
     channel_id = 845742634244898836
     channel = await client.fetch_channel(channel_id)  # Use fetch_channel instead of get_channel
-    
+    web_yoink()
     if os.path.exists('./game_info.yaml'):
         with open('./game_info.yaml', 'r') as file:
             game_info = yaml.safe_load(file)

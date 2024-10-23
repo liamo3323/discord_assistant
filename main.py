a135aa92f35de8005358105d83ad136df8c317ff
Liam <liam3323@gmail.com>
2024-10-23 12:31:06 +0100

    added embed

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import discord
 import logging
 import yaml
 import asyncio
-import time
+import datetime
 
 from web_yoinking import get_games_info
-from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,6 +18,19 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True 
 client = discord.Client(intents=intents)
+
+
+async def shutdown(ctx):
+    await ctx.send("Shutting down...")
+    await client.close()  # Gracefully close the bot
+
+
+
+async def web_yoink():
+    while True:
+        print("Yoinking...")
+        await get_games_info()
+        await asyncio.sleep(60)
 
 
 @client.event
@@ -36,18 +48,27 @@ async def on_ready():
         print("game_info.yaml does not exist")
 
 
-async def web_yoink():
-    while True:
-        print("Yoinking...")
-        await get_games_info()
-        await asyncio.sleep(60)
-
-
 @client.event
 async def on_message(message):
     print(message.content)
-    # channel.send("Hello, this message is from the bot!")
 
+    if message.author == client.user:
+        return
+
+    if message.content == '!a':
+        embed = discord.Embed(title="Game Price Tracking Added!",
+                      description="GAME has been added to the list of tracked games!",
+                      colour=discord.Color.blue(),
+                      timestamp=datetime.datetime.now())
+
+        embed.set_author(name="Silver Wolf")
+
+        embed.set_footer(text="Game Price Tracking")
+
+        await message.channel.send(embed=embed)
+
+    else:
+        await message.add_reaction('👍')
 
 async def main():
     asyncio.create_task(web_yoink())
